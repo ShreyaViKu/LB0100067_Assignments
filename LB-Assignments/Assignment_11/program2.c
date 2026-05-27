b14ea070a7ad315aaa1b5e53f80a3a5d70a3655a
shreya2004 <kulkarnishreya971@gMAIL.com>
2026-05-28 01:32:25 +0530

    Assignment 11 solution uploded

---
+++ LB-Assignments/Assignment_11/program2.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void RangeDisplayEven(int iStart, int iEnd)
+{
+    int iCnt = 0;
+
+    if(iStart > iEnd)
+    {
+        printf("Invalid range");
+    }
+    else
+    {
+        for(iCnt = iStart; iCnt <= iEnd; iCnt++)
+        {
+           if(iCnt % 2 == 0)
+           {
+            printf("%d \t",iCnt);
+           }
+        }
+    }
+
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter starting point : \n");
+    scanf("%d",&iValue1);
+
+    printf("Enter End point : \n");
+    scanf("%d",&iValue2);
+    
+    RangeDisplayEven(iValue1, iValue2);
+
+    return 0;
+}
+// Time Complexity : O(N)
---
+++ LB-Assignments/Assignment_11/program4.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+int RangeSumEven(int iStart, int iEnd)
+{
+    int iCnt = 0;
+    int iSum = 0;
+
+    if(iStart > iEnd || iStart < 0 || iEnd < 0)
+    {
+        return 0;
+    }
+    else
+    {
+        for(iCnt = iStart; iCnt <= iEnd; iCnt++)
+        {
+          if(iCnt % 2 == 0)
+          {
+            iSum += iCnt;
+          }
+        }
+    }
+    return iSum;
+
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0, iRet = 0;
+
+    printf("Enter starting point : \n");
+    scanf("%d",&iValue1);
+
+    printf("Enter End point : \n");
+    scanf("%d",&iValue2);
+    
+    iRet = RangeSumEven(iValue1, iValue2);
+
+    if(iRet == 0)
+    {
+        printf("Invalid range");
+    }
+    else
+    {
+        printf("Addition is %d",iRet);
+    }
+
+    return 0;
+}
+// Time Complexity : O(N)
