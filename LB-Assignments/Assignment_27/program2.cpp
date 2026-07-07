a4fba8607fec0e521c5f9dc6a534af8acf65c24a
shreya2004 <kulkarnishreya971@gMAIL.com>
2026-07-07 23:16:43 +0530

    assignment 27 uploded

---
+++ LB-Assignments/Assignment_27/program2.cpp
@@ -0,0 +1,61 @@
+/*
+iRow = 4   iCol = 4
+*  *  *  *
+*  *  *
+*  *
+*
+
+*/
+
+#include<iostream>
+using namespace std;
+
+class PatternPrint
+{
+    public: 
+        int iRow ;
+        int iCol ;
+
+        PatternPrint(int a, int b)
+        {
+            this->iRow = a;
+            this->iCol = b;
+        }
+
+        void Pattern()
+        {
+            if(iRow != iCol)
+            {
+                cout<<"Invalid input \nNumber of rows and column must be same\n";
+                return;
+            }
+            int i = 0, j = 0;
+
+            for(i = iRow; i >= 1; i--)
+            {
+                for(j= 1; j<=i; j++)
+                {
+                    cout<<"*\t";
+                }
+                cout<<"\n";
+            }
+        }
+
+};
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    cout<<"Enter number of rows : \n";
+    cin>>iValue1;
+
+    cout<<"Enter number of columns : \n";
+    cin>>iValue2;
+
+    PatternPrint pobj = PatternPrint(iValue1, iValue2);
+
+    pobj.Pattern();
+
+    return 0;
+}
---
+++ LB-Assignments/Assignment_27/program3.cpp
@@ -0,0 +1,73 @@
+/*
+iRow = 5   iCol = 5
+$  *  *  *  *
+#  $  *  *  *
+#  #  $  *  *
+#  #  #  $  *
+#  #  #  #  $
+
+*/
+
+#include<iostream>
+using namespace std;
+
+class PatternPrint
+{
+    public: 
+        int iRow ;
+        int iCol ;
+
+        PatternPrint(int a, int b)
+        {
+            this->iRow = a;
+            this->iCol = b;
+        }
+
+        void Pattern()
+        {
+            if(iRow != iCol)
+            {
+                cout<<"Invalid input \nNumber of rows and column must be same\n";
+                return;
+            }
+            int i = 0, j = 0;
+
+            for(i = 1; i <= iRow; i++)
+            {
+                for(j = 1; j <= iCol; j++)
+                {
+                    if(j == i)
+                    {
+                        cout<<"$\t";
+                    }
+                    else if (j < i)
+                    {
+                        cout<<"#\t";
+                    }
+                    else
+                    {
+                        cout<<"*\t";
+                    }  
+                }
+                cout<<"\n";
+            }
+        }
+
+};
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    cout<<"Enter number of rows : \n";
+    cin>>iValue1;
+
+    cout<<"Enter number of columns : \n";
+    cin>>iValue2;
+
+    PatternPrint pobj = PatternPrint(iValue1, iValue2);
+
+    pobj.Pattern();
+
+    return 0;
+}
